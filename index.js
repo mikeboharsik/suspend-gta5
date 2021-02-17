6d5e9901bf176aac8008a7be2dac806400653186
ganonscrub <mboharsik@gmail.com>
2021-02-17 14:51:16 -0500

    Reorganize repo structure

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const fs = require('fs');
 const path = require('path');
 const { execSync } = require('child_process');
 
-const html = fs.readFileSync(path.join(__dirname, './index.html'), 'utf-8');
-const psCommand = fs.readFileSync(path.join(__dirname, './psCommand.ps1'), 'utf-8');
+const html = fs.readFileSync(path.join(__dirname, './client/index.html'), 'utf-8');
+const psCommand = fs.readFileSync(path.join(__dirname, './ps/command.ps1'), 'utf-8');
 
 const port = 80;
 
